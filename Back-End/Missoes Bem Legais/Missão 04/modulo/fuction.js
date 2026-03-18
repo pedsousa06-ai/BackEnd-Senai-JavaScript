8401de2375a9dba79f58a58325ed713d84491d3d
PEDRO HENRIQUE DE SOUSA GOMES <25203650@CFP127ALUNO.LOCAL>
2026-03-18 11:26:03 -0300

    Salvando o projeto missao 04

---
+++ Back-End/Missoes Bem Legais/Missão 04/modulo/fuction.js
@@ -0,0 +1,22 @@
+/*************************************************************************************************************************************************************
+ * Funções Para missão 4 Muito Legal
+ * Autor: Pedro
+ * Data inicio: 18/03/2026
+ ************************************************************************************************************************************************************/
+
+const  listaDeEstados  = require("./estados_cidades")
+
+
+
+function getListaDeEstados() {
+
+    let lista = listaDeEstados
+
+    let siglas = []
+
+    lista.forEach(function(estado){
+        
+    })
+
+}
+getListaDeEstados()
